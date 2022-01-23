dd70b4784d99b6cb14bdbe928750c3d570260366
sc <sc@DESKTOP-USL3P9P>
2022-01-23 17:33:23 -0500

    Add project files.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var persona = new Persona() {
+                Nombre = "Scrafet",
+                Edad =35
+        };
+            return View(persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
